27e7c46281f47e81577aea86ccc7852565942805
Ariel <ariel.ras.90@gmail.com>
2024-04-28 12:14:53 -0300

    Se agregan las funciones personalizadas de Eda.ipynb al modulo de Functions.py

--- Functions.py
+++ Functions.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
+import math
+from scipy import stats
 from sklearn.metrics import confusion_matrix, accuracy_score, roc_auc_score, roc_curve
 
 class Metrics():
@@ -12,9 +15,9 @@ class Metrics():
         matrix = confusion_matrix(y_true, y_pred)
         sns.heatmap(matrix, fmt='.0f', annot=True, cmap='inferno')
         plt.title('Matriz de confusion')
-        plt.xlabel('Predict data')
-        #plt.xticks(ticks=[0.5,1.5], labels=[label_0, label_1])
+        plt.xlabel('Predict data')        
         plt.ylabel('True data')
+        #plt.xticks(ticks=[0.5,1.5], labels=[label_0, label_1])
         #plt.yticks(ticks=[0.5,1.5], labels=[label_0, label_1])
         plt.show()
 
@@ -29,3 +32,56 @@ class Metrics():
         plt.tight_layout()
         plt.show()
         mpl.style.use('default')
+
+
+class Correlations():
+    def __init__(self, df:pd.DataFrame, target_var:str, vars:list, vars_type:str):
+        self.df = df
+        self.target_var = target_var
+        self.vars = vars
+        self.vars_type = vars_type
+
+    #CREA UN DATAFRAME VACIO PARA ALOJAR LAS CORRELACIONES
+    def _get_correlation_dataframe(self) -> pd.DataFrame:
+        df_corr = pd.DataFrame(index=self.vars, columns=['correlation'])
+        return df_corr    
+
+    #DEVUELVE EL NUMERO MULTIPLO DE 10 SUPERIOR INMEDIATO AL VALOR MAXIMO DE LA SERIE
+    def _get_limit_value(self, values:pd.Series) -> float:
+        limit_value = values.abs().max()
+        limit_value = np.ceil(limit_value*10)/10
+        return limit_value
+
+    #FUNCION PARA PLOTEAR CORRELACION DE PUNTO BISERIAL
+    def plot_biserial_point(self):
+        df_corr = self._get_correlation_dataframe()
+        corr_list = [stats.pointbiserialr(self.df[self.target_var], self.df[var]) for var in self.vars]
+        for var, corr in zip(self.vars, corr_list):
+            df_corr.loc[var, 'correlation'] = corr.statistic
+        self._plot_correlation(df_corr)    
+
+    #FUNCION PARA PLOTEAR COEFICIENTE DE CONTINGENCIA
+    def plot_contingency_coef(self):
+        n = self.df.shape[0]
+        df_corr = self._get_correlation_dataframe()
+        for var in self.vars:
+            contingency_table = pd.crosstab(self.df[var], self.df[self.target_var])
+            chi_2 = stats.chi2_contingency(contingency_table)
+            df_corr.loc[var, 'correlation'] = math.sqrt(chi_2.statistic/(n + chi_2.statistic))            
+        self._plot_correlation(df_corr)
+
+    #FUNCION PARA PLOTEAR EL GRAFICO DE BARRAS
+    def _plot_correlation(self, df_corr:pd.DataFrame):
+        colors = ['#000E9E' if corr < 0 else '#3DB2DA' for corr in df_corr.correlation]
+        limit_value = self._get_limit_value(df_corr.correlation)
+        plt.style.use("bmh")
+        plt.figure(figsize=(8,5))  
+        sns.barplot(x=df_corr.correlation, y=df_corr.index, hue=df_corr.index, palette=colors)
+        plt.title(f'Correlacion de variable objetivo con variables {self.vars_type}', fontsize=14)
+        plt.xlabel('Coeficiente de correlacion')
+        plt.ylabel(f'Variables {self.vars_type}')
+        plt.xlim(-1*limit_value, limit_value)
+        plt.axvline(x=0, color='black', linestyle='-')
+        plt.tight_layout()
+        plt.show()
+        plt.style.use("default")
